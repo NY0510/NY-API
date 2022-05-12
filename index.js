d47630949f5679f931be5aeabc012cc9a3efb416
NY64 <namnyang0510@gmail.com>
2022-05-12 22:25:15 +0900

    🔥 URL에서 /v1 제거

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = config.port || 3000;
 // app.use("/", main);
 
 const nsfw = require("./routes/api/v1/nsfw");
-app.use("/v1/nsfw", nsfw);
+app.use("/nsfw", nsfw);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}!\n\nhttp://localhost:${port}\nhttp://127.0.0.1:${port}`);
