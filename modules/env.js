dfcfdb3d37997f230ee4ab27e42eab3aab0e7a2a
NY64 <namnyang0510@gmail.com>
2022-07-16 00:59:42 +0900

    ♻️ 코드 정리

--- modules/env.js
+++ modules/env.js
@@ -2,16 +2,4 @@ require("dotenv").config();
 
 module.exports = {
 	port: process.env.PORT,
-	hostname: process.env.HOSTNAME,
-
-	// message: {
-	// 	error: {
-	// 		failedToGetImage: { text: process.env.MESSAGE_ERROR_IMAGE_LOAD, code: process.env.CODE_ERROR_GET_DATA },
-	// 		failToGetData: {text: process.env.MESSAGE_ERROR_IMAGE_LOAD, code: process.env.CODE_ERROR_GET_DATA },
-	// 		unknownError: { text: process.env.MESSAGE_ERROR_UNKNOWN_ERROR, code: process.env.CODE_ERROR_UNKNOWN },
-	// 	},
-	// 	success: {
-	// 		imageLoaded: { text: process.env.MESSAGE_SUCCESS_IMAGE_LOAD, code: process.env.CODE_SUCCESS_GET_DATA },
-	// 	},
-	// },
 };
