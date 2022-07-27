2dce2f03443576deb034a313ad817a510445c884
Your Name <me@ny64.kr>
2022-07-27 12:45:03 +0900

    ⚡️ 라우팅 추가 자동화

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const asyncify = require("express-asyncify");
 
 const app = asyncify(express());
 const path = require("path");
+const fs = require("fs");
 const config = require(path.join(__dirname, "modules", "env.js"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const port = config.port || 3000;
+const apiVersion = config.apiVersion;
 
 app.use("/", require("./routes/main"));
-app.use("/nsfw", require("./routes/api/v1/nsfw"));
-app.use("/tjChart", require("./routes/api/v1/tjChart"));
+fs.readdirSync(path.join(__dirname, "routes", "api", `v${apiVersion}`)).forEach(file => {
+	if (file.endsWith(".js")) {
+		try {
+			const route = require(path.join(__dirname, "routes", "api", `v${apiVersion}`, file));
+			app.use(`/${file.split(".")[0]}`, route);
+			console.log(`Loaded Route: ./routes/api/v${apiVersion}/${file}`);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}!\n\nhttp://localhost:${port}\nhttp://127.0.0.1:${port}`);
--- modules/env.js
+++ modules/env.js
@@ -2,4 +2,5 @@ require("dotenv").config();
 
 module.exports = {
 	port: process.env.PORT,
+	apiVersion: process.env.API_V,
 };
