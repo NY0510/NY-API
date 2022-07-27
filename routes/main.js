2bbdc52cdffc3540d86100ecc27d325edf276a2a
Your Name <me@ny64.kr>
2022-07-27 12:29:49 +0900

    ✏️ 오타 수정

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
 	res.redirect("https://github.com/NY0510/NY-API", 301);
 });
 
-router.get("docs", (req, res) => {
+router.get("/docs", (req, res) => {
 	res.redirect("https://ny64.gitbook.io/ny-api/", 301);
 });
 
