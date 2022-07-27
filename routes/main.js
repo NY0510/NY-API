826ef000aab9e3e7831febdc6e564ee66a087883
Your Name <me@ny64.kr>
2022-07-27 12:28:29 +0900

    ✅ Docs 리다이렉트

--- routes/main.js
+++ routes/main.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
 	res.redirect("https://github.com/NY0510/NY-API", 301);
 });
 
+router.get("docs", (req, res) => {
+	res.redirect("https://ny64.gitbook.io/ny-api/", 301);
+});
+
 module.exports = router;
